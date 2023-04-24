5189725cd8395c0a377af7460469146f367c62bd
KairosMP <109192850+KairosMP@users.noreply.github.com>
2023-04-24 09:50:22 -0400

    inicio de front

---
+++ front-end/web/src/App.js
@@ -0,0 +1,14 @@
+import Layout from "./components/layout";
+import GlobalStyles from "./styles/GlobalStyles";
+
+
+function App() {
+  return (
+    <>
+    <Layout />
+    <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
